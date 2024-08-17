598e23dcad69f351a9d75842451536566d43aac7
Diogo Dourado <dcdourado@gmail.com>
2024-08-17 15:39:46 -0300

    Local file is now loaded from root folder

--- src/main/java/br/com/diogoritmos/mediaaction/file/LoadLocalMediaFileImpl.java
+++ src/main/java/br/com/diogoritmos/mediaaction/file/LoadLocalMediaFileImpl.java
@@ -4,8 +4,8 @@ import org.apache.commons.io.input.CountingInputStream;
 import org.apache.tika.Tika;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class LoadLocalMediaFileImpl implements LoadMediaFile {
@@ -14,38 +14,26 @@ public class LoadLocalMediaFileImpl implements LoadMediaFile {
     private static final long MAX_SIZE = 1_073_741_824; // 1GB in bytes
 
     @Override
-    public MediaFile loadFile(String path) throws FileNotFoundException {
-        var beforeMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-
-        var resource = getClass().getClassLoader().getResource(path);
-        if (resource == null) {
-            throw new FileNotFoundException("File not found");
-        }
-
-        var resourcePath = resource.getFile();
-        System.out.println("Resource path: " + resourcePath);
+    public MediaFile loadFile(String path) {
+        var fullPath = Paths.get(path);
+        var resourcePath = fullPath.toString();
 
         try (FileInputStream fis = new FileInputStream(resourcePath)) {
             final var cis = new CountingInputStream(fis);
             var mimeType = tika.detect(cis);
             System.out.println("Detected MIME type: " + mimeType);
-            System.out.println("Bytes read until MIME type detection: " + cis.getByteCount());
 
             if (!SUPPORTED_MIMETYPES.contains(mimeType)) {
                 throw new IllegalArgumentException("Unsupported media type");
             }
 
             byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = cis.read(buffer)) != -1) {
+            while (cis.read(buffer) != -1) {
                 if (cis.getByteCount() > MAX_SIZE) {
                     throw new IOException("File size exceeds the maximum allowed");
                 }
             }
-            System.out.println("Bytes read in total: " + cis.getByteCount());
-
-            long afterMemory = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
-            System.out.println("Memory consumed after load: " + (afterMemory - beforeMemory) + " bytes");
+            System.out.println("Media content length: " + cis.getByteCount());
 
             return new MediaFile(
                     resourcePath,

bfb2ff8a853351abd9544aa1587df22ec5c5de49
Diogo Dourado <dcdourado@gmail.com>
2024-08-17 13:05:30 -0300

    Configure env vars

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     id("application")
+    id("co.uzzu.dotenv.gradle") version "4.0.0"
 }
 
 group = "br.com.diogoritmos"
@@ -13,6 +14,14 @@ repositories {
 dependencies {
     implementation("org.apache.tika:tika-core:2.4.0")
 
+    // Requires explicit configuration
+    implementation(mapOf(
+        "group" to "com.microsoft.cognitiveservices.speech",
+        "name" to "client-sdk",
+        "version" to "1.40.0",
+        "ext" to "jar"
+    ))
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -23,4 +32,9 @@ application {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<JavaExec> {
+    environment("SPEECH_KEY", System.getenv("SPEECH_KEY"))
+    environment("SPEECH_REGION", System.getenv("SPEECH_REGION"))
 }

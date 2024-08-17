32270243e8bad4cae09c915bb5dc83a1d12dc48f
Diogo Dourado <dcdourado@gmail.com>
2024-08-17 17:14:16 -0300

    Add logs in offload

--- src/main/java/br/com/diogoritmos/mediaaction/offload/OffloadMediaContentInConsoleImpl.java
+++ src/main/java/br/com/diogoritmos/mediaaction/offload/OffloadMediaContentInConsoleImpl.java
@@ -6,8 +6,10 @@ import br.com.diogoritmos.mediaaction.transcript.TranscriptBlock;
 public class OffloadMediaContentInConsoleImpl implements OffloadMediaContent {
     @Override
     public void offload(MediaTranscript transcript) {
+        System.out.println("Offloading media content in console...");
         for (TranscriptBlock block : transcript.getTranscriptBlocks()) {
             System.out.println(block.getText());
         }
+        System.out.println("Media content successfully offloaded");
     }
 }
